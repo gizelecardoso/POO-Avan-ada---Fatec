3bcc97e5162853b999ff6dd803c37fdece231adb
Gizele Santos <gizele.ccardoso@gmail.com>
2020-02-12 14:31:57 -0300

    Alterando teste - estudo JUnit refatorando

--- Fatec_poo/src/br/com/poo/aula1/test/TesteTest.java
+++ Fatec_poo/src/br/com/poo/aula1/test/TesteTest.java
@@ -12,25 +12,35 @@ import br.com.poo.aula1.Tipo4;
 class TesteTest {
 
 	@Test
-	public void testValorAReceber() {
+	public void testValorAReceberTipo1() {
 		
 		Tipo1 tipo1 = new Tipo1(1500);
 		assertEquals(1500, tipo1.getValorAReceber());
+	
+	}
+	
+	@Test
+	public void testValorAReceberTipo2() {
 		
 		Tipo2 tipo2 = new Tipo2(1500, 0.1);
-		
 		double salarioAtual = tipo2.getSalario() + (tipo2.getSalario() * tipo2.getPercentualComissao());
-		
-		assertEquals(salarioAtual, tipo2.getValorAReceber());
+		assertEquals(salarioAtual, tipo2.getValorAReceber());	
+	}
+	
+	@Test
+	public void testValorAReceberTipo3() {
 		
 		Tipo3 tipo3 = new Tipo3(1500, 0.1, 0.1);
-		
 		double salarioAtual2 = tipo3.getSalario() + (tipo3.getSalario() * tipo3.getPercentualComissao()) + tipo3.getBonus();
-		
 		assertEquals(salarioAtual2, tipo3.getValorAReceber());
 		
-		Tipo4 tipo4 = new Tipo4(1200);
+	}
+	
+	
+	@Test
+	public void testValorAReceberTipo4() {
 		
+		Tipo4 tipo4 = new Tipo4(1200);
 		assertEquals(tipo4.getBonus(), tipo4.getValorAReceber());	
 		
 	}
